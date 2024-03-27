17436283b9a87814dce3a6950c204bd2dab28093
abelthomas <abelmathews2005@gmail.com>
2024-03-27 16:01:33 -0500

    Worked on the Book.java.

--- Library.java
+++ Library.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Library {
     public static void main(String[] args) throws Exception {
-        Item myItem = new Item("abc", "01-20-2002");
+        Item myItem = new Item("abc", "05-29-2024");
         System.out.println(myItem);
     }
 }
