2050e326992af70eef217bc9f423ac90992455da
abelthomas <abelmathews2005@gmail.com>
2024-03-29 18:14:46 -0500

    added the override to Cd, Book and dvd also added the library app file that debbie gave us as a starter

--- inventory/Book.java
+++ inventory/Book.java
@@ -1,38 +1,43 @@
-//package inventory;
-//
-//public class Book extends Item {
-//
-//    private String author;
-//    private String bookgenre;
-//
-//    public Book(String title, String invdate) throws Exception {
-//        super(title, invdate);
-//    }
-//
-//    public Book(String title, String invdate, String author, String bookgenre) {
-//        super(title, invdate, author, bookgenre);
-//        this.author = author;
-//        this.bookgenre = bookgenre;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//
-//    }
-//
-//    public String getgenre() {
-//        return bookgenre;
-//    }
-//
-//    public void setauthor(){
-//        this.author = author;
-//    }
-//
-//    public void setgenre(String bookgenre){
-//        this.bookgenre =  bookgenre;
-//    }
-//
-//
-//}
-//
-//
+
+package inventory;
+
+public class Book extends Item {
+
+    private String author;
+    private String bookgenre;
+
+    public Book(String title, String invdate) throws Exception {
+        super(title, invdate);
+    }
+
+    public Book(String title, String invdate, String author, String bookgenre) {
+        super(title, invdate, author, bookgenre);
+        this.author = author;
+        this.bookgenre = bookgenre;
+    }
+
+    public String getAuthor() {
+        return author;
+
+    }
+
+    public String getgenre() {
+        return bookgenre;
+    }
+
+    public void setauthor(){
+        this.author = author;
+    }
+
+    public void setgenre(String bookgenre){
+        this.bookgenre =  bookgenre;
+    }
+    @Override
+    public void displayItem(){
+        super.displayItem();
+        System.out.printf("%-15s %-10s\n", author, bookgenre);
+
+    }
+
+}
+
--- inventory/Cd.java
+++ inventory/Cd.java
@@ -1,40 +1,48 @@
-//
-//package inventory;
-//
-//public class Cd extends Item {
-//
-//    private String artist;
-//    private String musicGenre;
-//
-//    public Cd(String title, String invdate) throws Exception {
-//        super(title, invdate);
-//    }
-//
-//    public Cd(String title, String invdate, String artist, String musicGenre) {
-//        super(title, invdate, artist, musicGenre);
-//        this.artist = artist;
-//        this.musicGenre = musicGenre;
-//    }
-//
-//    public String getArtist() {
-//        return artist;
-//
-//    }
-//
-//    public String getMusicGenre() {
-//        return musicGenre;
-//    }
-//
-//    public void setArtist(){
-//        this.artist = artist;
-//    }
-//
-//    public void setMusicGenre(String musicGenre){
-//        this.musicGenre =  musicGenre;
-//    }
-//
-//
-//}
-//
-//
-//
+
+
+package inventory;
+
+public class Cd extends Item {
+
+    private String artist;
+    private String musicGenre;
+
+    public Cd(String title, String invdate) throws Exception {
+        super(title, invdate);
+    }
+
+    public Cd(String title, String invdate, String artist, String musicGenre) {
+        super(title, invdate, artist, musicGenre);
+        this.artist = artist;
+        this.musicGenre = musicGenre;
+    }
+
+    public String getArtist() {
+        return artist;
+
+    }
+
+    public String getMusicGenre() {
+        return musicGenre;
+    }
+
+    public void setArtist(){
+        this.artist = artist;
+    }
+
+    public void setMusicGenre(String musicGenre){
+        this.musicGenre =  musicGenre;
+    }
+    @Override
+    public void displayItem(){
+        super.displayItem();
+        System.out.printf("%-15s %-10s\n", artist, musicGenre);
+
+    }
+
+
+}
+
+
+
+
